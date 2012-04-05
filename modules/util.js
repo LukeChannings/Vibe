a105049dd3119a1c901a7108098d5fe6b390b8ea
Luke Channings <thefuzzball2005@gmail.com>
2012-04-05 17:47:25 +0100

    Compatibility changes for IE8.

--- modules/util.js
+++ modules/util.js
@@ -7,7 +7,7 @@ define(function(){
 	/**
 	 * Prototypes.
 	 */
-	 
+		
 	// forEach.
 	if ( typeof Array.prototype.forEach == "undefined" )
 	{
@@ -24,38 +24,43 @@ define(function(){
 	}
 	
 	// setAttributes.
-	if ( typeof HTMLElement.prototype.setAttributes == "undefined" )
+	if ( typeof Element.prototype.setAttributes == "undefined" )
 	{
-		HTMLElement.prototype.setAttributes = function(attributes){
+		Element.prototype.setAttributes = function(attributes){
 			
+			var self = this;
+				
 			if ( attributes instanceof Array )
 			{
-				
-				var self = this;
-				
 				attributes.forEach(function(attribute){
-				
+					
 					self.setAttribute(attribute[0],attribute[1]);
-				
+					
 				});
 			}
-			
+			else if ( typeof attributes == "object" )
+			{
+				for ( var i in attributes )
+				{
+					self.setAttribute(i,attributes[i]);
+				}
+			}
 		}
 	}
 	
 	// removeNode.
-	if ( typeof HTMLElement.prototype.removeNode == "undefined" )
+	if ( typeof Element.prototype.removeNode == "undefined" )
 	{
-		HTMLElement.prototype.removeNode = function()
+		Element.prototype.removeNode = function()
 		{
 			this.parentNode.removeChild(this);
 		}
 	}
 	
 	// removeChildren.
-	if ( typeof HTMLElement.prototype.removeChildren == "undefined" )
+	if ( typeof Element.prototype.removeChildren == "undefined" )
 	{
-		HTMLElement.prototype.removeChildren = function()
+		Element.prototype.removeChildren = function()
 		{
 			while ( this.firstChild )
 			{
@@ -65,9 +70,9 @@ define(function(){
 	}
 	
 	// toggleClass.
-	if ( typeof HTMLElement.prototype.toggleClass == "undefined" )
+	if ( typeof Element.prototype.toggleClass == "undefined" )
 	{
-		HTMLElement.prototype.toggleClass = function(name)
+		Element.prototype.toggleClass = function(name)
 		{
 			var regex = new RegExp(name);
 		
@@ -81,6 +86,31 @@ define(function(){
 				else this.className += ' ' + name;
 			}
 		}
+	}
+	
+	// Array indexOf. (for IE <= 8)
+	// From MDC - https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf#Compatibility
+	if (!Array.prototype.indexOf)
+	{
+	  Array.prototype.indexOf = function(elt /*, from*/)
+	  {
+	    var len = this.length >>> 0;
+	
+	    var from = Number(arguments[1]) || 0;
+	    from = (from < 0)
+	         ? Math.ceil(from)
+	         : Math.floor(from);
+	    if (from < 0)
+	      from += len;
+	
+	    for (; from < len; from++)
+	    {
+	      if (from in this &&
+	          this[from] === elt)
+	        return from;
+	    }
+	    return -1;
+	  };
 	}
 	
 	/**
@@ -111,18 +141,28 @@ define(function(){
 			}
 		
 		},
-		addStylesheet : function(url){
+		registerStylesheet : function(url){
 		
-			document.head.appendChild(function(){
-			
-				var link = document.createElement('link');
-			
-				link.setAttributes([['rel','stylesheet'],['type','text/css'],['href',url]]);
-			
-				return link;
-			
-			}());
+			if ( ! window.registeredStylesheets ) window.registeredStylesheets = [];
 		
+			if ( window.registeredStylesheets.indexOf(url) == -1)
+			{
+				(document.head || document.getElementsByTagName("head")[0]).appendChild(function(){
+				
+					var link = document.createElement('link');
+				
+					link.setAttributes({
+						'rel' : 'stylesheet',
+						'type' : 'text/css',
+						'href' : url
+					});
+				
+					return link;
+				
+				}());
+				
+				window.registeredStylesheets.push(url);
+			}
 		},
 		Browser : {
 			isMobile : function(){
