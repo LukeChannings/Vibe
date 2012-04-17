630ab45cc4891db54f7ea3c960c2d00132e69e9e
Luke Channings <thefuzzball2005@gmail.com>
2012-04-18 00:24:00 +0100

    Add settings and playlist models.

---
+++ modules/Model/Playlist.js
@@ -0,0 +1,11 @@
+/**
+ * PlaylistModel
+ * @description Object that contains the playlist data and performs Api interactions.
+ */
+define(['util'],function(){
+
+	var PlaylistModel = function(){}
+	
+	return PlaylistModel;
+
+});
