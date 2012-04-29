3ac73074b11998c90320b866366e710391240472
Luke Channings <thefuzzball2005@gmail.com>
2012-04-29 21:41:28 +0100

    Added a placeholder shim for IE.

---
+++ modules/UI/Widget/Placeholder/Placeholder.js
@@ -0,0 +1,49 @@
+/**
+ * Placeholder
+ * @description Placeholder shim for non-compliant browsers. (IE.)
+ */
+define(['util'],function(util){
+
+	var UIPlaceholderWidget = function(node, placeholder) {
+	
+		if ( node instanceof Element && typeof placeholder == 'string' )
+		{
+			
+			node.value = placeholder;
+			
+			node.addClass('placeholder');
+			
+			util.addListener(node,'focus',function(e){
+			
+				var target = e.target || e.srcElement;
+				
+				if ( target.value == placeholder )
+				{
+					target.value = '';
+					
+					target.removeClass('placeholder');
+				}
+			
+			});
+			
+			util.addListener(node,'blur',function(e){
+			
+				var target = e.target || e.srcElement;
+			
+				if ( target.value == '' )
+				{
+					target.value = placeholder;
+					
+					target.addClass('placeholder');
+				}
+			
+			});
+			
+		}
+		else throw util.error('Invalid parameters.');
+	
+	}
+	
+	return UIPlaceholderWidget;
+
+});
