054a5cf83b250ca207aa747bb8e90cc2f5bd8f63
Luke Channings <thefuzzball2005@gmail.com>
2012-04-30 09:21:43 +0100

    Rewrite controller and using Model/Init for User Interface initialisation.

--- modules/controller.js
+++ modules/controller.js
@@ -1,145 +1,95 @@
 /**
- * MusicMe
- * @description Official MusicMe Web App.
+ * Vibe
+ * @description Official Vibe Web App.
  */
+(function(){
 
-// requirejs configuration.
-require.config({baseUrl: './modules/'});
-
-require(['dependencies/domReady','Model/Settings','util','Api/MusicMe'], function (domReady, Settings, util, Api) {
-
-	domReady(function(){
+	// create a controller object to contain important properties that we don't want in the global scope.
+	var Controller = function() {
 	
-		// make a global settings instance.
-		window.settings = new Settings();
+		var self = this,
+			dependencies = [
+				'dependencies/domReady',
+				'util','Model/Settings',
+				'UI/Settings/Settings',
+				'Api/Vibe',
+				'Model/Init'
+			];
 	
-		// get the musicme element.
-		var musicme = document.getElementById('MusicMe'),
-			api;
+		// requirejs configuration.
+		require.config({baseUrl: './modules/'});
+	
+		require(dependencies, function (domReady, util, ModelSettings, UISettings, Api, ModelInit) {
 		
-		var init = function()
-		{
-			if ( settings.get('host') && settings.get('port') )
-			{
-				// make an Api instance.
-				api = new Api();
+			domReady(function(){
 			
-				// listen for connection.
-				api.once('connected', initUI);
-					
-				// listen for error.
-				api.on('error', promptConnectionString);
+				// initial variables.
+				var vibeRootElement = self.vibeRootElement = document.getElementById('Vibe'),
+					settings = self.settings = new ModelSettings(),
+					uiSettings = self.uiSettings = new UISettings(settings),
+					api = self.api = new Api(settings);
+			
+				// check if the browser is mobile.
+				if ( util.Browser.isMobile() )
+				{
+					// add the mobile stylesheet additions if it is.
+					util.registerStylesheet("./vibe.mobile.css");
+				}
 				
-				// listen for disconnection.
-				api.on('disconnected',function(){
-					
-					console.log("Api lost connection.");
-					
+				// check for SVG support.
+				if ( util.Browser.HasSupport.svg() && ! util.Browser.isIE() )
+				{
+					// add the SVG class to the body.
+					document.body.addClass('svg');
+				}
+			
+				// set the vibe version.
+				settings.set('version', util.getMetaContent('vibe-version'));
+			
+				// listen for a connection event.
+				api.once('connected',function(){
+				
+					if ( util.Browser.isMobile() )
+					{
+						ModelInit.mobile.call(self);
+					}
+					else
+					{
+						ModelInit.desktop.call(self);
+					}
+				
 				});
-			}
-			else
-			{
-				promptConnectionString();
-			}
-			
-		}
-		
-		var initUI = function(){
-			
-			// fetch previous node.
-			var collection = document.getElementById('UICollection'),
-				playlist = document.getElementById('UIPlaylist'),
-				overlay = document.getElementById('ModalDialogueOverlay');
-			
-			// check for previous nodes and remove them.
-			if ( collection ) collection.removeNode();
-			if ( playlist ) playlist.removeNode();
-			if ( overlay ) overlay.removeNode();
-			
-			// require the UI partials.
-			require(['UI/Collection/Collection','UI/Playlist/Playlist','Model/Playlist'],function(UICollection,UIPlaylist,ModelPlaylist){
-			
-				var playlist = new UIPlaylist({appendTo : musicme, useControlBar : true, useInfoBar : true});
-			
-				playlistModel = new ModelPlaylist({withUI : playlist, withApi : api});
-			
-				var collection = new UICollection({withApi : api, appendTo : musicme, usingSearch : true, usingInfoBar : true, dragAndDropElement: playlist.node, withRootType : 'genre'});
-			
-				collection.on('itemSelected',function(item){
 				
-					playlistModel.add(item.type,item.id);
+				// listen for an error.
+				api.once('error',function(){
+				
+					uiSettings.firstrun.call(self,function(){
+					
+						api.connect();
+					
+					},"Unable to connect.","<p>Vibe was unable to connect to '" + settings.get('host') + ':' + settings.get('port') + "'.</p> <p>Please check the Vibe Server is running, and that the address for the server is correct.</p><p>If the address is incorrect, please change it in the form below. Otherwise, press 'Go'.</p>");
+				
+				});
+				
+				api.once('firstrun',function(){
+				
+					// run the first run dialogue.
+					uiSettings.firstrun.call(self,function(){
+					
+						// attempt to reconnect using the new settings.
+						api.connect();
+					
+					});
 				
 				});
 			
 			});
-			
-		}
 		
-		var promptConnectionString = function(){
-		
-			var MDD = {};
-			
-			if ( arguments.callee.done )
-			{
-				MDD.title = "Unable to connect.";
-				
-				MDD.body = "MusicMe was unable to establish a connection to " + settings.get('host') + ':' + settings.get('port') + ". If the specified Url is incorrect feel free to change it below, otherwise please troubleshoot your MusicMe server.";
-				
-			}
-			else
-			{
-				MDD.title = "Welcome to MusicMe";
-				
-				MDD.body = "It looks like this is your first time using MusicMe on this computer. Before we can get started, we need the details of your MusicMe server. Please enter the details in the form below and press 'Go.'";
-			}
-			
-			MDD.form = {
-				'name' : 'gettingstarted',
-				'inputs' : [{
-					'title' : "Host:",
-					'name' : 'host',
-					'placeholder' : settings.get('host') || 'localhost'
-				},{
-					'title' : "Port:",
-					'name' : 'port',
-					'placeholder' : settings.get('port') || 6232
-				}]
-			};
-		
-			MDD.buttons = {'Go' : function(){
-			
-				// get the settings from the form.
-				var host = document.forms['gettingstarted']['host'].value || document.forms['gettingstarted']['host'].placeholder || 'localhost';
-				var port = parseInt(document.forms['gettingstarted']['port'].value) || document.forms['gettingstarted']['port'].placeholder ||6232
-			
-				// set the settings.
-				settings.set('host', host);
-				settings.set('port', port);
-			
-				// close the form.
-				this.close();
-				
-				// reconnect the Api.
-				if ( api ) api.connect();
-				else init();
-
-			}};
-		
-			// require the modal dialogue component.
-			require(['UI/ModalDialogue/ModalDialogue'],function(dialogue){
-			
-				dialogue.createSingle(MDD);
-			
-			});
-		
-			arguments.callee.done = true;
-		
-		}
-		
-		var disconnected = function(){}
-		
-		init();
-		
-	});
-
-});
+		});
+	
+	}
+	
+	// call the controller.
+	new Controller();
+	
+})();
