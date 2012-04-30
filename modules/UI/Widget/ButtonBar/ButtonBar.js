d6c1ed78a55f15cb034b85319f8dfcca1985b7af
Luke Channings <thefuzzball2005@gmail.com>
2012-04-30 09:24:35 +0100

    Added ButtonBar module to abstract a horizontal button menu from UIPlaylist.

---
+++ modules/UI/Widget/ButtonBar/ButtonBar.js
@@ -0,0 +1,54 @@
+define(['require','util'],function(require, util){
+
+	util.registerStylesheet(require.toUrl('./ButtonBar.css'));
+
+	var UIButtonBarWidget = function(options) {
+	
+		if ( typeof options == 'object' ) 
+		{
+			
+			var node = this.node = util.createElement({
+				'tag' : 'ol',
+				'appendTo' : options.appendTo || document.body,
+				'customClass' : 'UIButtonBarWidget'
+			});
+			
+			if ( options.buttons instanceof Array )
+			{
+				options.buttons.forEach(function(button){
+				
+					var li = util.createElement({'tag' : 'li', appendTo : node});
+				
+					var item = util.createElement({'tag' : 'button', appendTo : li});
+				
+					item.addClass('UIButtonWidget');
+				
+					if ( ! button.iconButton ) item.innerHTML = button.text || button.title || '';
+				
+					if ( button.customClass ) item.addClass(button.customClass);
+				
+					if ( typeof button.titleText == 'string' ) item.setAttribute('title',button.titleText);
+				
+					if ( typeof button.callback == 'function' )  util.addListener(item, 'click', button.callback);
+				
+					if ( button.floatRight ) li.addClass('right');
+				
+					if ( button.isIcon ) item.addClass('icon');
+				
+				});
+			}
+			else {
+				
+				throw util.error("UIButtonBarWidget requires an array of button objects.");
+				
+			};
+			
+		}
+		
+		else return false;
+		
+	}
+
+	return UIButtonBarWidget;
+
+});
