6e3d6372ba195bd278e633bb22867fde6a645988
Luke Channings <thefuzzball2005@gmail.com>
2012-05-08 18:13:09 +0100

    Break vibe launcher.

--- modules/UI/Playlist/Playlist.js
+++ modules/UI/Playlist/Playlist.js
@@ -2,74 +2,73 @@
  * MusicMe Playlist
  * @description Provides the Playlist view and mutator methods.
  */
-define(['require','util','dependencies/EventEmitter','UI/Widget/ButtonBar/ButtonBar'],function(require, util, EventEmitter, ButtonBar) {
+define(['require','util','dependencies/EventEmitter','UI/Widget/ButtonBar/ButtonBar'], function(require, util, EventEmitter, ButtonBar) {
 
 	// register the view stylesheet.
-	util.registerStylesheet(require.toUrl('./Playlist.css'));
+	util.registerStylesheet(require.toUrl('./Playlist.css'))
 	
 	var initControlBar = function(ButtonBarButtons) {
 	
-		var controlBar = this.controlBar = util.createElement({ 'tag' : 'div', 'customClass' : 'controlBar', appendTo : this.header });
+		var controlBar = this.controlBar = util.createElement({ 'tag' : 'div', 'customClass' : 'controlBar', appendTo : this.header }),
+			self = this
 	
-		var self = this;
-	
-		this.node.addClass('usingControlBar');
+		this.node.addClass('usingControlBar')
 	
 		var buttons = new ButtonBar({
 			appendTo : controlBar,
 			buttons : ButtonBarButtons
-		});
+		})
 	
 	}
 
 	var initInfoBar = function() {
 	
-		var infoBar = this.infoBar = util.createElement({ 'tag' : 'div', 'customClass' : 'infoBar', appendTo : this.node });
+		var infoBar = this.infoBar = util.createElement({ 'tag' : 'div', 'customClass' : 'infoBar', appendTo : this.node })
 	
-		this.node.addClass('usingInfoBar');
+		this.node.addClass('usingInfoBar')
 	
 	}
 
 	var createPlaylistRow = function(itemDefinition,useColumns) {
 	
-		var item = util.createElement({ 'tag' : 'ol' });
+		var item = util.createElement({ 'tag' : 'ol' })
 	
-		var columns = {};
+		var columns = {}
 	
-		for ( var i in itemDefinition )
-		{
-			var column = document.createElement('li');
+		for ( var i in itemDefinition ) {
+			var column = document.createElement('li')
 			
-			if ( i == 'tracklength' && typeof itemDefinition[i] == 'number' ) itemDefinition[i] = util.formatTime(itemDefinition[i]);
+			if ( i == 'tracklength' && typeof itemDefinition[i] == 'number' ) itemDefinition[i] = util.formatTime(itemDefinition[i])
 			
-			column.innerHTML = itemDefinition[i];
+			column.innerHTML = itemDefinition[i]
 			
-			column.addClass(i);
+			column.addClass(i)
 			
-			columns[i] = column;
+			columns[i] = column
 		}
 	
-		if ( useColumns instanceof Array )
-		{
-			for ( var i = 0; i < useColumns.length; i++ )
-			{
-				if ( columns[useColumns[i]] ) {
-					item.appendChild(columns[useColumns[i]]);
+		if ( useColumns instanceof Array ) {
+		
+			useColumns.forEach(function(column) {
+			
+				if ( columns[column] ) {
+				
+					item.appendChild(columns[column])
+				
 				}
-			}
+			
+			})
 		}
-		else
-		{
-			for ( var i in columns ) item.appendChild(columns[i]);
-		}
+		
+		else  for ( var i in columns ) item.appendChild(columns[i])
 	
-		return item;
+		return item
 	
 	}
 
 	var drawLegend = function() {
 	
-		if ( this.header.children[1] ) this.header.removeChild(this.header.children[1]);
+		if ( this.header.children[1] ) this.header.removeChild(this.header.children[1])
 	
 		// create the legend.
 		var legends = this.legends = {
@@ -81,15 +80,15 @@ define(['require','util','dependencies/EventEmitter','UI/Widget/ButtonBar/Button
 			trackno: "#",
 			trackof: "# Tracks",
 			year: "Year"
-		};
+		}
 	
-		var legend = createPlaylistRow(legends, this.options.useColumns);
+		var legend = createPlaylistRow(legends, this.options.useColumns)
 			
 		// add the legend class.
-		legend.addClass('legend');
+		legend.addClass('legend')
 		
 		// append the legend to the header.
-		this.header.appendChild(legend);
+		this.header.appendChild(legend)
 	
 	}
 
@@ -97,86 +96,86 @@ define(['require','util','dependencies/EventEmitter','UI/Widget/ButtonBar/Button
 	var UIPlaylist = function(options) {
 	
 		// check for an options object.
-		var options = this.options = ( typeof options == 'object' ) ? options : {};
+		var options = this.options = ( typeof options == 'object' ) ? options : {}
 
-		this.playingNode = undefined;
-		this.selectedNodes = [];
+		this.playingNode = undefined
+		this.selectedNodes = []
 
 		// set a default useColumns.
-		options.useColumns = ( options.useColumns instanceof Array ) ? options.useColumns : ['trackno','trackname','albumname','artistname','tracklength'];
+		options.useColumns = ( options.useColumns instanceof Array ) ? options.useColumns : ['trackno','trackname','albumname','artistname','tracklength']
 
 		// create the root UIPlaylist node.
 		var node = this.node = util.createElement({
 			tag : 'div',
 			id : 'UIPlaylist',
 			appendTo : ( options.appendTo instanceof Element ) ? options.appendTo : document.body
-		});
+		})
 
 		// create a node to contain the list legend and optionally the controls bar.
-		var header = this.header = util.createElement({'tag' : 'div', 'appendTo' : node});
+		var header = this.header = util.createElement({'tag' : 'div', 'appendTo' : node})
 
 		// check for useControlBar buttons. If they exist then init the control bar.
-		if ( options.useControlBar instanceof Array ) initControlBar.call(this, options.useControlBar);
+		if ( options.useControlBar instanceof Array ) initControlBar.call(this, options.useControlBar)
 
 		// create the legend.
-		drawLegend.call(this);
+		drawLegend.call(this)
 
 		// define a container for the playlist ordered-list.
 		var listContainer = util.createElement({
 			'tag' : 'div',
 			'customClass' : 'listContainer',
 			'appendTo' : node
-		});
+		})
 		
 		// create the playlist ordered-list.
-		var list = this.list = util.createElement({'tag' : 'ol', 'appendTo' : listContainer});
+		var list = this.list = util.createElement({'tag' : 'ol', 'appendTo' : listContainer})
 		
 		util.doubleClick(list, function(element) {
 		
-			element.parentNode.parentNode.addClass('selected');
+			element.parentNode.parentNode.addClass('selected')
 		
 		},function(element) {
 		
-			console.log(element);
+			console.log(element)
 		
-		});
+		})
 		
 		// include the info bar if the option is specified.
-		if ( options.useInfoBar ) initInfoBar.call(this);
+		if ( options.useInfoBar ) initInfoBar.call(this)
 	
 	}
 	
 	UIPlaylist.prototype.redraw = function(items, redrawLegend) {
 	
-		var self = this;
+		var self = this
 		
-		this.list.removeChildren();
+		this.list.removeChildren()
 		
-		if ( redrawLegend ) this.redrawLegend();
+		if ( redrawLegend ) this.redrawLegend()
 		
 		items.forEach(function(item) {
 		
-			var node = util.createElement({'tag' : 'li', appendTo : self.list});
+			var node = util.createElement({'tag' : 'li', appendTo : self.list})
 			
-			var row = createPlaylistRow(item, self.options.useColumns);
+			var row = createPlaylistRow(item, self.options.useColumns)
 		
-			node.appendChild(row);
+			node.appendChild(row)
 		
-		});
+		})
 	
 	}
 	
 	UIPlaylist.prototype.redrawLegend = function() {
 		
-		drawLegend.call(this);
+		drawLegend.call(this)
 		
 	}
 	
 	
 	// use EventEmitter.
-	EventEmitter.augment(UIPlaylist.prototype);
+	EventEmitter.augment(UIPlaylist.prototype)
 		
 	// define UIPlaylist module.
-	return UIPlaylist;
+	return UIPlaylist
 
-});
+})
--- vibe.js
+++ vibe.js
@@ -1,96 +1,53 @@
-/**
- * Vibe
- * @description Official Vibe Web App.
- */
 (function(){
 
-	// create a controller object to contain important properties that we don't want leaking into the global scope.
+	// define the vibe controller.
 	var Vibe = function() {
 	
-		var self = this,
-			dependencies = [
-				'dependencies/domReady',
-				'util',
-				'Model/Settings',
-				'UI/Settings/Settings',
-				'Api/Vibe',
-				'Model/Init'
-			];
+		// define Vibe base dependencies.
+		var dependencies = [
+				'domReady', // DOM Ready require.js plugin.
+				'util', // require utility methods.
+				'Model/Settings', // Settings Model.
+				'UI/Settings/Settings', // Settings UI.
+				'Api/Vibe', // Vibe Api.
+				'Model/Init' // UI Initialisation.
+			],
+			self = this
 	
-		// requirejs configuration.
-		require.config({baseUrl: './modules/'});
+		// set require.js base url.
+		require.config({baseUrl: './modules/'})
 	
-		require(dependencies, function (domReady, util, ModelSettings, UISettings, Api, ModelInit) {
+		// fetch the dependencies...
+		require(dependencies, function(domReady, util, ModelSettings, UISettings, Api, Init) {
 		
-			domReady(function(){
+			// wait for the DOM to load...
+			domReady(function() {
 			
-				// initial variables.
-				var vibeRootElement = self.vibeRootElement = document.getElementById('Vibe'),
-					settings = self.settings = new ModelSettings(),
-					uiSettings = self.uiSettings = new UISettings(settings),
-					api = self.api = new Api(settings);
+				// get the root node.
+				var rootNode = document.getElementById('Vibe')
 			
-				// check if the browser is mobile.
-				if ( util.Browser.isMobile() )
-				{
-					// add the mobile stylesheet additions if it is.
-					util.registerStylesheet("./vibe.mobile.css");
-				}
+				// instantiate settings model.
+				var settings = self.settings = new ModelSettings()
 				
-				// check for SVG support.
-				if ( util.Browser.HasSupport.svg() && ! util.Browser.isIE() )
-				{
-					// add the SVG class to the body.
-					document.body.addClass('svg');
-				}
+				// instantiate settings user interface.
+				var uiSettings = self.uiSettings = new UISettings(settings)
 			
-				// set the vibe version.
-				settings.set('version', util.getMetaContent('vibe-version'));
+				// instantiate the Vibe Api.
+				var api = self.api = new Api(settings)
 			
-				// listen for a connection event.
-				api.once('connected',function(){
-				
-					if ( util.Browser.isMobile() )
-					{
-						ModelInit.mobile.call(self);
-					}
-					else
-					{
-						ModelInit.desktop.call(self);
-					}
-				
-				});
-				
-				// listen for an error.
-				api.once('error',function(){
-				
-					uiSettings.firstrun.call(self,function(){
-					
-						api.connect();
-					
-					},"Unable to connect.","<p>Vibe was unable to connect to '" + settings.get('host') + ':' + settings.get('port') + "'.</p> <p>Please check the Vibe Server is running, and that the address for the server is correct.</p><p>If the address is incorrect, please change it in the form below. Otherwise, press 'Go'.</p>");
-				
-				});
-				
-				api.once('firstrun',function(){
-				
-					// run the first run dialogue.
-					uiSettings.firstrun.call(self,function(){
-					
-						// attempt to reconnect using the new settings.
-						api.connect();
-					
-					});
-				
-				});
-				
-			});
+				// if the browser is mobile, load the mobile stylesheet.
+				if ( util.Browser.isMobile() ) util.registerStylesheet('vibe.mobile.css')
+			
+				// detect SVG support.
+				if ( util.Browser.HasSupport.svg() && ! util.Browser.isIE() ) document.body.addClass('svg')
+			
+			})
 		
-		});
+		})
 	
 	}
 	
-	// start up the engines...
-	new Vibe();
+	// start your engines...
+	new Vibe()
 	
 })();
