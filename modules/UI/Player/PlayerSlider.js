eff69ffe147b1eaf02207d0a81b6a854ed003176
Luke Channings <thefuzzball2005@gmail.com>
2012-05-18 00:37:22 +0100

    Add modules.

---
+++ modules/UI/Player/PlayerSlider.js
@@ -0,0 +1,69 @@
+define(['util', 'UI/Widget/DragDealer/DragDealer', 'dependencies/EventEmitter'], function(util, DragDealer, EventEmitter) {
+
+	var UIPlayerSlider = function(options) {
+	
+		var options = typeof options == 'object' ? options : {},
+			self = this,
+			node = this.node,
+			slider = this.slider,
+			trackTime = this.trackTime,
+			trackDuration = this.trackDuration
+
+			// construct the UIPlayerSlider node.
+			node = util.createElement({
+				'tag' : 'div',
+				'id' : 'UIPlayerSlider',
+				'appendTo' : options.appendTo || document.body
+			})
+			
+			slider = util.createElement({
+				'tag' : 'div',
+				'customClass' : 'dragdealer',
+				'appendTo' : node,
+				'children' : [{
+					'tag' : 'div',
+					'customClass' : 'handle red-bar'
+				}]
+			})
+			
+			window.slider = new DragDealer(slider)
+
+	}
+	
+	UIPlayerSlider.prototype.update = function(update) {
+	
+		if ( typeof update == 'object' ) {
+		
+			if ( update.trackPosition ) {
+			
+				// update position.
+			
+			}
+		
+			if ( update.bufferPosition ) {
+			
+				// update the time.
+			
+			}
+			
+			if ( update.trackDuration ) {
+			
+				// update the track duration.
+			
+			}
+		
+			if ( update.trackProgress ) {
+			
+				// update the track progress.
+			
+			}
+		
+		}
+	
+	}
+
+	EventEmitter.augment(UIPlayerSlider.prototype)
+
+	return UIPlayerSlider
+
+})
